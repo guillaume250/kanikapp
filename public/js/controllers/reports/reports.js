fbaad97bc69488809a02c9519242ce58ac1a0dd2
unknown <guillaume@savics.org>
2019-12-02 23:32:53 -0500

    Linting 2

--- public/js/controllers/reports/reports.js
+++ public/js/controllers/reports/reports.js
@@ -1,9 +1,8 @@
-angular.module("reportsCtrl", []).controller('reportsCtrl',function($scope){
-
-$scope.title = "Features under development"
-$scope.contents = [
-  "Pie Chart of Number of Customers per month, the pie chart shows customers by name to check the customer frequency",
-  "Advanced search customer by type of subscription, pay as you go or monthly subscription",
-  "Single chart for new, confirmed and canceled bookings"
-]
+angular.module("reportsCtrl", []).controller("reportsCtrl", function($scope) {
+  $scope.title = "Features under development";
+  $scope.contents = [
+    "Pie Chart of Number of Customers per month, the pie chart shows customers by name to check the customer frequency",
+    "Advanced search customer by type of subscription, pay as you go or monthly subscription",
+    "Single chart for new, confirmed and canceled bookings"
+  ];
 });
--- public/js/controllers/users/users.js
+++ public/js/controllers/users/users.js
@@ -1,7 +1,4 @@
-angular.module("usersCtrl", []).controller('usersCtrl',function($scope){
-  $scope.title = "Features under development"
-  $scope.contents = [
-    "User management system",
-    "User access control system"
-  ]
+angular.module("usersCtrl", []).controller("usersCtrl", function($scope) {
+  $scope.title = "Features under development";
+  $scope.contents = ["User management system", "User access control system"];
 });
