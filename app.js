5a21766e509c2dfe92a1036f43d9c4b9375f6995
unknown <guillaume@savics.org>
2019-12-02 22:18:59 -0500

    Refactoring beginning

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-'use strict';
-const express = require('express');
-//var methodOverride  = require('method-override');
-var path = require('path');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+"use strict";
+const express = require("express");
+//const methodOverride  = require('method-override');
+const path = require("path");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-//var db = 'mongodb://guil12:guil12@ds041821.mlab.com:41821/kanikadb';
-var db_alt = 'mongodb://fabrice:fabrice@ds127065.mlab.com:27065/kanikapp'
+//const db = 'mongodb://guil12:guil12@ds041821.mlab.com:41821/kanikadb';
+const db_alt = "mongodb://fabrice:fabrice@ds127065.mlab.com:27065/kanikapp";
 mongoose.connect(db_alt);
 
 //app.use(methodOverride());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-extended:true
-}))
-app.use('/assests', express.static(__dirname + '/public'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use("/assests", express.static(__dirname + "/public"));
 //app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 // Routes
-require('./app/routes')(app);
+require("./app/routes")(app);
 if (module === require.main) {
   // [START server]
   // Start the server
@@ -30,7 +30,6 @@ if (module === require.main) {
     console.log(`Kanika listening on port ${port}`);
     console.log(`Kanika url: http://localhost:5000/`);
     console.log(`Big Shaq... You don't know`);
-
   });
   // [END server]
 }
--- app/routes.js
+++ app/routes.js
@@ -1,77 +1,81 @@
-var path = require('path');
-var app = require('../app');
-var _ = require('underscore');
-var aclRoutes = require('../resources/dictionary/app/routes.json');
+var path = require("path");
+var app = require("../app");
+var _ = require("underscore");
+var aclRoutes = require("../resources/dictionary/app/routes.json");
 
 var controllers = {};
-controllers.angular = function (req, res) {res.sendFile(path.join(__dirname, '../public', 'index.html')); console.log("Main Route Called")};
-controllers.bookings = require('./controllers/bookings');
-controllers.customers = require('./controllers/customers');
-controllers.users = require('./controllers/users');
+controllers.angular = function(req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+  console.log("Main Route Called");
+};
+controllers.bookings = require("./controllers/bookings");
+controllers.customers = require("./controllers/customers");
+controllers.users = require("./controllers/users");
 
 var routes = [
-     // Initial route
-    {
-        path: _.findWhere(aclRoutes, {id: 0}).uri,
-        httpMethod: _.findWhere(aclRoutes, {id: 0}).method,
-        middleware: [controllers.angular]
-    },
+  // Initial route
+  {
+    path: _.findWhere(aclRoutes, { id: 0 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 0 }).method,
+    middleware: [controllers.angular]
+  },
 
-    // Booking routes
-    {
-        path: _.findWhere(aclRoutes, {id: 1}).uri,
-        httpMethod: _.findWhere(aclRoutes, {id: 1}).method,
-        middleware: [controllers.bookings.api.list]
-    },
-    {
-        path: _.findWhere(aclRoutes, {id: 2}).uri,
-        httpMethod: _.findWhere(aclRoutes, {id: 2}).method,
-        middleware: [controllers.bookings.api.new]
-    },
-    {
-        path: _.findWhere(aclRoutes, {id: 3}).uri,
-        httpMethod: _.findWhere(aclRoutes, {id: 3}).method,
-        middleware: [controllers.bookings.api.update]
-    },
-    {
-        path: _.findWhere(aclRoutes, {id: 4}).uri,
-        httpMethod: _.findWhere(aclRoutes, {id: 4}).method,
-        middleware: [controllers.bookings.api.delete]
-    },
+  // Booking routes
+  {
+    path: _.findWhere(aclRoutes, { id: 1 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 1 }).method,
+    middleware: [controllers.bookings.api.list]
+  },
+  {
+    path: _.findWhere(aclRoutes, { id: 2 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 2 }).method,
+    middleware: [controllers.bookings.api.new]
+  },
+  {
+    path: _.findWhere(aclRoutes, { id: 3 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 3 }).method,
+    middleware: [controllers.bookings.api.update]
+  },
+  {
+    path: _.findWhere(aclRoutes, { id: 4 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 4 }).method,
+    middleware: [controllers.bookings.api.delete]
+  },
 
-      // Users routes
-      {
-          path: _.findWhere(aclRoutes, {id: 5}).uri,
-          httpMethod: _.findWhere(aclRoutes, {id: 5}).method,
-          middleware: [controllers.bookings.api.list]
-      },
-      {
-          path: _.findWhere(aclRoutes, {id: 6}).uri,
-          httpMethod: _.findWhere(aclRoutes, {id: 6}).method,
-          middleware: [controllers.bookings.api.new]
-      }
+  // Users routes
+  {
+    path: _.findWhere(aclRoutes, { id: 5 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 5 }).method,
+    middleware: [controllers.bookings.api.list]
+  },
+  {
+    path: _.findWhere(aclRoutes, { id: 6 }).uri,
+    httpMethod: _.findWhere(aclRoutes, { id: 6 }).method,
+    middleware: [controllers.bookings.api.new]
+  }
+];
 
-]
-
-module.exports = function (app) {
-    _.each(routes, function (route) {
-        var args = _.flatten([route.path, route.middleware]);
-        switch (route.httpMethod.toUpperCase()) {
-            case 'GET':
-                app.get.apply(app, args);
-                break;
-            case 'POST':
-                app.post.apply(app, args);
-                break;
-            case 'PUT':
-                app.put.apply(app, args);
-                break;
-            case 'DELETE':
-                app.delete.apply(app, args);
-                break;
-            default:
-                throw new Error('Invalid HTTP method specified for route ' + route.path);
-                break;
-        }
-    });
+module.exports = function(app) {
+  _.each(routes, function(route) {
+    var args = _.flatten([route.path, route.middleware]);
+    switch (route.httpMethod.toUpperCase()) {
+      case "GET":
+        app.get.apply(app, args);
+        break;
+      case "POST":
+        app.post.apply(app, args);
+        break;
+      case "PUT":
+        app.put.apply(app, args);
+        break;
+      case "DELETE":
+        app.delete.apply(app, args);
+        break;
+      default:
+        throw new Error(
+          "Invalid HTTP method specified for route " + route.path
+        );
+        break;
+    }
+  });
 };
