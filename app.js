f0bdf1572965574634755876945e8827485dd02a
unknown <guillaume@savics.org>
2019-12-05 22:32:46 -0500

    Refactor 12

--- app.js
+++ app.js
@@ -16,14 +16,9 @@ app.use("/assests", express.static(__dirname + "/public"));
 
 // Routes
 require("./app/routes")(app);
-if (module === require.main) {
-  // [START server]
-  // Start the server
-  const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
-    console.log(`Kanika listening on port ${port}`);
-    console.log(`Kanika url: http://localhost:5000/`);
-    console.log(`Big Shaq... You don't know`);
-  });
-  // [END server]
-}
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Kanika listening on port ${port}`);
+  console.log(`Kanika url: http://localhost:5000/`);
+});
