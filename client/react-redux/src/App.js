59d57e8b2bde3278391774b607725dfb71f2006c
guillaume250 <guillaumesayinzoga@gmail.com>
2020-08-20 16:22:43 -0400

    JellyFish exercise

--- client/react-redux/src/App.js
+++ client/react-redux/src/App.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const datasetLabels = {
+    name: "Name",
+    allocation: "Allocation",
+    allocationPercentage: "Allocation %"
+  };
+
+  const dataset = [
+    {
+      id: 1,
+      name: "Product",
+      allocation: 0.1,
+      allocationPercentage: 10
+    },
+    {
+      id: 2,
+      name: "Infrastructure",
+      allocation: 0.7,
+      allocationPercentage: 70
+    },
+    {
+      id: 3,
+      name: "Reactive",
+      allocation: 0.15,
+      allocationPercentage: 15
+    },
+    {
+      id: 4,
+      name: "Grooming",
+      allocation: 0.05,
+      allocationPercentage: 5
+    }
+  ];
+
+  //
+  // // IGNORE, used to render as an example
+  // document.getElementById("datasetLabels").innerText = `const datasetLabels = ${JSON.stringify(datasetLabels)}`;
+  // document.getElementById("dataset").innerText = `const dataset = ${JSON.stringify(dataset)}`;
+
+  const [count, setCount] = useState(0);
+
+  const mapTitles = Object.values(datasetLabels).map(item => <th>{item}</th>);
+  const titlesKeys = Object.keys(datasetLabels);
+
+  const map = dataset.map(item => {
+    return (
+      <tr key={item.id}>
+        {titlesKeys.map((key, i) => {
+          return <td>{item[key]}</td>;
+        })}
+      </tr>
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tr>{mapTitles}</tr>
+          {map}
+        </table>
       </header>
     </div>
   );
